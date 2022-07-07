3745656c5ea461393c7d7d21730957771c57b056
Daniel Osorio <daniel.osorio.w@usach.cl>
2022-07-07 00:26:33 -0400

    Connection

--- backend/settings.py
+++ backend/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'enunciados',
         'USER': 'postgres',
-        'PASSWORD': 'admin',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'DATABASE_PORT': '5432'
     }
