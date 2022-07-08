976206288a85fdcff0e4358c21a3125fb0e90ff4
Daniel Osorio <daniel.osorio.w@usach.cl>
2022-07-08 17:51:27 -0400

    Connection

--- backend/settings.py
+++ backend/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
+# Default primary key field typee
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
